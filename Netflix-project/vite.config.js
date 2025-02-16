a5abd81d6a561030888e709a0dfe08c3a55903d6
nahi-t <nahomketema553@gmail.com>
2025-02-16 21:32:30 +0300

    vit

--- Netflix-project/vite.config.js
+++ Netflix-project/vite.config.js
@@ -1,23 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-pro-/",
-})
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://api.themoviedb.org/3', // HTTPS is essential!
-//         changeOrigin: true, // This is absolutely required for CORS
-//         rewrite: (path) => path.replace(/^\/api/, ''), // Remove the /api prefix
-//       },
-//     },
-//   },
-// });
+  root: ".",  // Ensure this is set correctly
+  build: {
+    outDir: "dist",
+  }
+});
