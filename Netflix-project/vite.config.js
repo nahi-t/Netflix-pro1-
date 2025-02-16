80b62cac28f9b4f4a2bc4b45b4e6012643aa4afc
nahi-t <nahomketema553@gmail.com>
2025-02-16 18:02:27 +0300

    Fixed vite.config.js base path for GitHub Pages

--- Netflix-project/vite.config.js
+++ Netflix-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Netflix-pro-",
+  base: "/Netflix-pro-/",
 })
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
