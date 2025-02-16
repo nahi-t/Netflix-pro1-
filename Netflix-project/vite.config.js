4f61179389fc0c88959898c94db85a47f2a93c0c
nahi-t <nahomketema553@gmail.com>
2025-02-16 17:25:54 +0300

    deploy

--- Netflix-project/vite.config.js
+++ Netflix-project/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Netflix-pro-",
 })
 // import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
